1befc0a4c11b03623f94a69de89185ff753cd7ec
kshitij shirsat <shirsatkshitij1847@gmail.com>
2026-05-25 17:50:57 +0530

    first commit added app.js express code

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/deployment', (req, res) => {
+  res.json({ message: 'Deployment successful!' })
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
